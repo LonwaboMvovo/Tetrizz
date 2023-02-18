30065a0664d1f47eba9fc6b19c33984927124d9c
unknown <lomvovo022@student.wethinkcode.com>
2023-02-18 09:47:48 +0200

    added game timer for max 60 frames/sec and set up user input commands

--- game.py
+++ game.py
@@ -48,9 +48,24 @@ def play_game():
     while True:
         # Check player inputs/events
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                print("GAME OVER!!!")
                 pygame.quit()
                 exit()
+            
+            if event.type == tetromino_drop_timer:
+                print("DROP PIECE DOWN 1")
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    print("MOVE PIECE TO THE LEFT 1")
+                elif event.key == pygame.K_RIGHT:
+                    print("MOVE PIECE TO THE RIGHT 1")
+                elif event.key == pygame.K_UP:
+                    print("ROTATE PIECE CLOCKWISE")
+                elif event.key == pygame.K_DOWN:
+                    print("SOFT DROP - MOVE PIECE DOWN 1")
+
 
         # If bag is empty get a new bag of 7 random pieces
         if len(seven_bag) == 0:
@@ -62,13 +77,32 @@ def play_game():
         # Get starting position of current tetromino
         block_pos = get_tetromino_coords(chosen_tetromino)
 
+        # Current tetromino drop iteration
+        for _ in range(20):
+            for pos in block_pos:
+                playfield_grid[pos[0]][pos[1]][0] = 1
+                playfield_grid[pos[0]][pos[1]][1] = chosen_tetromino
+
         
         # Update screen/display
         pygame.display.update()
+        # max set to 60 frames/sec
+        clock.tick(60)
 
 
 # inits bruv
 pygame.init()
+clock = pygame.time.Clock()
+
+# Set Window title
+pygame.display.set_caption("Tetrizz")
+
+# Set window icon
+icon_surface = pygame.Surface((32, 32))
+icon_surface.fill((139,0,139))
+pygame.display.set_icon(icon_surface)
+
+# Set background
 screen = pygame.display.set_mode((350, 700))
 screen_bg_colour = (42,43,46)
 screen.fill(screen_bg_colour)
@@ -80,13 +114,9 @@ for x in range(35, 400, 35):
 for y in range(35, 800, 35):
     pygame.draw.line(screen, "black", (0, y), (400, y), width = 1)
 
-# Set window icon
-icon_surface = pygame.Surface((32, 32))
-icon_surface.fill((139,0,139))
-pygame.display.set_icon(icon_surface)
-
-# Set Window title
-pygame.display.set_caption("Tetrizz")
+# Timers:
+tetromino_drop_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(tetromino_drop_timer, 500)
 
 if __name__ == "__main__":
     play_game()
